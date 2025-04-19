9abd5214d6843c6d45967e13d5e116cf58c75eeb
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2025-04-19 12:38:50 +0530

    demo __proto__ and its connection to prototype
    Demonstrated how __proto__ links an object to its constructor's prototype, reinforcing the prototype chain concept.

---
+++ week2/prototype.js/2proto.js
@@ -0,0 +1,5 @@
+// What's __proto__?
+
+// the __proto__ is a hidden link inside every object that points to its constructor's prototype
+
+// console.log(user1.__proto__ === Person.prototype); // true
