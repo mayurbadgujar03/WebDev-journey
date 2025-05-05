d54c8d15015e90dddf3dcfde46615662b57b7e52
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2025-05-05 11:23:13 +0530

    Spelling fixes

--- week3/PracticeFormScratch/model/User.model.js
+++ week3/PracticeFormScratch/model/User.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
-    verficationToken: {
+    verificationToken: {
         type: String,
     },
     resetPasswordToken: {
