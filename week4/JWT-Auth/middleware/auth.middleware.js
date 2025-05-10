26a9603ad175e39bfa426ca2617988932785cbfe
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2025-05-10 19:33:49 +0530

    feat: implement JWT-based authentication

---
+++ week4/JWT-Auth/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const isLoggedIn = async (req, res, next) => {
+
+    const token = req.cookies?.token;
+
+    try {
+        
+        if(!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Cookie not found",
+            });
+        };
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+}
+
+export default isLoggedIn;
