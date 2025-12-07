dc871f331c44a184adce929d676597a1e4140428
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2025-12-07 09:52:41 +0530

    Type Annotations and Inference in TypeScript

---
+++ week9/04_TS/typesInTs.ts
@@ -0,0 +1,18 @@
+// type annotations
+// type inference
+
+let drink = "chai"
+
+let cupsNum = Math.random() > 0.5 ? 10 : 5;
+
+let cupNumStr = Math.random() > 0.5 ? 10 : '5';
+
+let chaiFlavour:string = "masala chai"
+chaiFlavour = "Ginger tea"
+
+let chaiOrder:boolean
+
+
+// string
+// number
+// boolean
