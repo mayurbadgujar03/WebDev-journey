44dc6562d566285d622ab9d59ff8a9b884438406
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2025-12-13 12:52:29 +0530

    Object discussion in Typescript

---
+++ week10/09_TS/objects.ts
@@ -0,0 +1,105 @@
+const chai = {
+    name: "Masala chai",
+    price: 20,
+    isHot: true
+}
+
+let tea: {
+    name: string;
+    price: number;
+    isHot: boolean
+}
+
+tea = {
+    name: "Ginger Tea",
+    price: 25,
+    isHot: true
+}
+
+type Tea = {
+    name: string;
+    price: number;
+    ingredients: string[]
+}
+
+const adrakChai: Tea = {
+    name: "Adrak Chai",
+    price: 25,
+    ingredients: ["ginger", "tea leaves"]
+}
+
+type Cup = {size: string};
+let smallCup: Cup = {size: "200ml"}
+
+let bigCup = {size: "500ml", material: "steel"}
+
+smallCup = bigCup
+
+type Brew = {brewTime: number}
+const coffee = {brewTime: 5, beans: "Arabica"}
+const chaiBrew: Brew = coffee
+
+type User = {
+    username: string;
+    password: string
+}
+
+const u: User = {
+    username: "ChaiCode",
+    password: "123"
+}
+
+type Item= {name: string, quantity: number}
+type Address = {street: string, pin: number}
+
+type Order = {
+    id: string;
+    items: Item[];
+    address: Address
+}
+
+type Chai = {
+    name: string;
+    price: number;
+    isHot: boolean
+}
+
+const updateChai = (updates: Partial<Chai>) = {
+    console.log("updating chai with", updates);
+}
+
+updateChai({price: 25})
+updateChai({isHot:false})
+updateChai({})
+
+
+type ChaiOrder = {
+    name?:string;
+    quantity?:number
+}
+
+const placeOrder = (order: Required<ChaiOrder>) => {
+    console.log(order);
+}
+
+placeOrder({
+    name: "Masala Chai",
+    quantity: 2
+})
+
+
+type BasicChaiInfo = Pick<Chai, 'name' | 'price'>;
+
+const chaiInfo: BasicChaiInfo = {
+    name: "Lemon tea",
+    price: 30
+}
+
+type ChaiNew = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    secretIngredients: string;
+};
+
+type PublicChai = Omit<Chai, "secretIngredients">;
